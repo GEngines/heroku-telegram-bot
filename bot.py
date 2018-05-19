1ae7509b2e355050e30f87681e4f670383a19d84
Bharath Rao <GEngines@users.noreply.github.com>
2018-05-19 12:45:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
+import redis
+import os
+import telebot
+# import some_api_lib
+# import ...
 
+# Example of your code beginning
+#           Config vars
+token = os.environ['TELEGRAM_TOKEN']
+some_api_token = os.environ['SOME_API_TOKEN']
+#             ...
 
-
+# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
+r = redis.from_url(os.environ.get("REDIS_URL"))
 
 
 import requests
 import datetime
 from time import sleep
-import telepot
 
 greet_bot = telepot.Bot("570531436:AAGhlmqiyehM7hS_GzmvS9_pdnhUN96VFV8")
 reply_bot = telepot.Bot("600394117:AAFWzJN8ybJTo5IKhEyYtPKxNjkGBvxVugA")
